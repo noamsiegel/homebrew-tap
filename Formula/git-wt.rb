457b14bcc26a8408fffea47d52d0876712df148f
Noam Siegel <noam@noamsiegel.com>
2026-05-23 14:38:30 -0700

    git-wt: bump to v0.2.1 (version-sync hotfix)

--- Formula/git-wt.rb
+++ Formula/git-wt.rb
@@ -1,8 +1,8 @@
 class GitWt < Formula
   desc "Parallel-safe git worktree CLI for agentic coding sessions"
   homepage "https://github.com/noamsiegel/git-wt"
-  url "https://github.com/noamsiegel/git-wt/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "c4010aa17adcfb71eab6ddd5321bcea52606156a2c44f88a8dae2c81f85244b6"
+  url "https://github.com/noamsiegel/git-wt/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "c5dbae7f83a987e63e939f8d2929f46e5eb6e1c1708a23ab013194aafd81c200"
   license "MIT"
   head "https://github.com/noamsiegel/git-wt.git", branch: "main"
 
